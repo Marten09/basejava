3b8ea6603419c286c42e73c8db2b55d71c6e20b7
Marten09 <leshadyuckarev631@gmail.com>
2023-03-14 19:59:43 +0300

    + new class ResumeTestData

--- test/com/urise/webapp/storage/ResumeTestData.java
+++ test/com/urise/webapp/storage/ResumeTestData.java
@@ -2,27 +2,33 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.*;
 
-import java.util.*;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 public class ResumeTestData {
-    private final Map<SectionType, AbstractSection> section= new EnumMap<>(SectionType.class);
-    private final Map<ContactType,String> contact = new EnumMap<>(ContactType.class);
-    private void setContacts() {
+    private static final Map<SectionType, AbstractSection> section = new EnumMap<>(SectionType.class);
+    private static final Map<ContactType, String> contact = new EnumMap<>(ContactType.class);
+
+    private static void setContacts() {
         contact.put(ContactType.PHONENUMBER, "++7(921) 855-0482");
         contact.put(ContactType.SKYPENICKNAME, "skype:grigory.kislin");
-        contact.put(ContactType.EMAIL," gkislin@yandex.ru");
+        contact.put(ContactType.EMAIL, " gkislin@yandex.ru");
     }
-    private void setSection(){
-        section.put(SectionType.PERSONAL,new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры."));
-        section.put(SectionType.OBJECTIVE,new TextSection("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям"));
-        section.put(SectionType.ACHIEVEMENT,new ListSection(Collections.singletonList("Организация команды и успешная реализация Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + Vaadin проект для комплексных DIY смет\n" +
+
+    private static void setSection() {
+        section.put(SectionType.PERSONAL, new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры."));
+        section.put(SectionType.OBJECTIVE, new TextSection("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям"));
+        section.put(SectionType.ACHIEVEMENT, new ListSection(Collections.singletonList("Организация команды и успешная реализация Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + Vaadin проект для комплексных DIY смет\n" +
                 "С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 3500 выпускников.\n" +
                 "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.\n" +
                 "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.\n" +
                 "Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.\n" +
                 "Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).\n" +
                 "Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.")));
-        section.put(SectionType.QUALIFICATIONS,new ListSection(Collections.singletonList("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2\n" +
+        section.put(SectionType.QUALIFICATIONS, new ListSection(Collections.singletonList("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2\n" +
                 "Version control: Subversion, Git, Mercury, ClearCase, Perforce\n" +
                 "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB\n" +
                 "Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy\n" +
@@ -36,14 +42,14 @@ public class ResumeTestData {
                 "администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer\n" +
                 "Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектрирования, архитектурных шаблонов, UML, функционального программирования\n" +
                 "Родной русский, английский \"upper intermediate\"")));
-//        section.put(SectionType.EXPERIENCE,new OrganizationsSection(Collections.singletonList("Alcatel, \n",
-//                "Инженер по аппаратному и программному тестированию","Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).")));
-//        section.put(SectionType.EDUCATION,new OrganizationsSection(Collections.singletonList("Заочная физико-техническая школа при МФТИ, \n",
-//                "Закончил с отличием")));
+        section.put(SectionType.EXPERIENCE,new OrganizationSection(List.of(new Organization("Alcatel","https://javaops.ru/", List.of(new Period("Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).",LocalDate.of(1997, 7, 1), LocalDate.of(2005, 1, 1)))))));
+        section.put(SectionType.EDUCATION,new OrganizationSection(List.of(new Organization("Заочная физико-техническая школа при МФТИ","https://mipt.ru/",List.of(new Period("","Закончил с отличием",LocalDate.of(1984,9,1),LocalDate.of(1987,6,1)))))));
     }
 
     public static void main(String[] args) {
         Resume resume = new Resume("Grigorii Kislin");
         System.out.println(resume.getFullName());
+        setContacts();
+        setSection();
     }
 }
