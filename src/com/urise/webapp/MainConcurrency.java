389431123ffed982e7e1b63d3cd777de0b94c67f
Marten09 <leshadyuckarev631@gmail.com>
2023-04-07 17:52:23 +0300

    hm11 try

---
+++ src/com/urise/webapp/MainConcurrency.java
@@ -0,0 +1,109 @@
+package com.urise.webapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainConcurrency {
+    public static final int THREADS_NUMBER = 10000;
+    private int counter;
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                //throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(mainConcurrency.counter);
+
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+    }
+
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + " got lock1 monitor");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();;
+                }
+                System.out.println(Thread.currentThread().getName() + " trying to get lock2 monitor");
+                synchronized (lock2) {
+                    System.out.println(Thread.currentThread().getName() + " got lock2 monitor");
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + " got lock2 monitor");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();;
+                }
+                System.out.println(Thread.currentThread().getName() + " trying to get lock1 monitor");
+                synchronized (lock1) {
+                    System.out.println(Thread.currentThread().getName() + " got lock1 monitor");
+                }
+            }
+        }).start();
+    }
+
+    private synchronized void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+        counter++;
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
+}
