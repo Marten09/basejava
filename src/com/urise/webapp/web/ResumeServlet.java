c7a8625c94e572e56ee9e101437db73b885113a8
Marten09 <leshadyuckarev631@gmail.com>
2023-04-27 17:09:08 +0300

    HM 15 try

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,10 +1,23 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.storage.Storage;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.getInstance().getStorage();
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
@@ -27,14 +40,23 @@ public class ResumeServlet extends HttpServlet {
                 "  <tr>\n" +
                 "    <td>uuid</td>\n" +
                 "    <td>full_name</td>\n" +
-                "  </tr>\n" +
-                "  <tr>\n" +
-                "    <td>75781ce7-8eae-46ac-a736-3fac40e9ecd7</td>\n" +
-                "    <td>Name1</td>\n" +
-                "  </tr>\n" +
-                "</table>\n");
-
-
+                "  </tr>\n");
+//                "  <tr>\n" +
+//                "    <td>75781ce7-8eae-46ac-a736-3fac40e9ecd7</td>\n" +
+//                "    <td>Name1</td>\n" +
+//                "  </tr>\n" +
+//                "</table>\n")
+        for (Resume resume : storage.getAllSorted()) {
+            response.getWriter().write("<tr>\n" +
+                    "        <td>" + resume.getUuid() + "</td>\n" +
+                    "        <td>" + resume.getFullName() + "</td>\n" +
+                    "    </tr>\n");
+        }
+        response.getWriter().write("</section>\n" +
+                "</table>\n" +
+                "</body>\n" +
+                "</html>\n");
+        response.getWriter().close();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
